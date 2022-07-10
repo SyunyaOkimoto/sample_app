80c3f4ba8987ead282897de4aed3404a6260e7b9
ShunyaOkimoto <105156188+SyunyaOkimoto@users.noreply.github.com>
2022-07-10 15:35:10 +0000

    [sample_app]6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,20 @@
 class ListsController < ApplicationController
-
   def new
     @list = List.new
   end
   
-  def index
-    @lists = List.all
-  end
-  
-  def show
+  def edit
     @list = List.find(params[:id])
   end
   
-  
-  def create
-    list = List.new(list_params)
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
     redirect_to list_path(list.id)  
   end
 
-
   private
-  # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)  
   end
 end
