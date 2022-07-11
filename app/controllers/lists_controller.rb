5161abc74bc365b57af511655a56cbc6a9df19da
ShunyaOkimoto <105156188+SyunyaOkimoto@users.noreply.github.com>
2022-07-11 13:40:52 +0000

    [sample_app]7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,19 +2,25 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body, :image)  
+    params.require(:list).permit(:title, :body, :image)
   end
 end
